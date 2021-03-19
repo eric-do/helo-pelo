dc2435772843d32e94a79742bc79e51ac83dea77
Eric Do <ericdo.617@gmail.com>
2021-03-18 17:43:05 -0700

    Fix ESLint issues for tests

--- frontend/src/__tests__/TopAppBar.test.tsx
+++ frontend/src/__tests__/TopAppBar.test.tsx
@@ -3,14 +3,15 @@
  */
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { within } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import TopAppBar from '../navigation/TopAppBar';
 import { BACKEND_URL } from '../config';
+import type { Tag, Ride, Comment } from '../types';
 
-const tags = [
+const tags: Tag[] = [
   {
     name: 'test',
     tag_count: 435,
@@ -21,9 +22,77 @@ const tags = [
   },
 ];
 
+const comments: Comment[] = [
+  {
+    comment: 'This is a test comment with no tags',
+    id: 44,
+    created_at: '2021-03-09T21:20:29.035720',
+    user: {
+      id: 2,
+      username: 'test_user1',
+      email: 'user1@mail.com',
+      is_active: true,
+      is_superuser: true,
+    },
+  },
+  {
+    comment: 'This comment has #wonderful tags',
+    id: 46,
+    created_at: '2021-03-11T04:49:05.976540',
+    user: {
+      id: 3,
+      username: 'test_user2',
+      email: 'user2@mail.com',
+      is_active: true,
+      is_superuser: false,
+    },
+  },
+  {
+    comment: 'This another comment',
+    id: 46,
+    created_at: '2021-03-11T04:49:05.976540',
+    user: {
+      id: 3,
+      username: 'test_user2',
+      email: 'user2@mail.com',
+      is_active: true,
+      is_superuser: false,
+    },
+  },
+];
+
+const rides: Ride[] = [
+  {
+    id: 10,
+    description: 'The ride from hell. Get on this and never get off.',
+    ride_id: '3863ad9c30af4635b3c2be1aea766fff',
+    difficulty_estimate: 7.7485,
+    duration: 1800,
+    fitness_discipline_display_name: 'Tread Bootcamp',
+    image_url:
+      'https://s3.amazonaws.com/peloton-ride-images/dda7988466b2257d89928788fd9711dc08ed95ba/img_1613524141_d4b7d06e1c6044b09af859abff8f21fd.png',
+    instructor_id: '1b79e462bd564b6ca5ec728f1a5c2af0',
+    title: '30 min of hell',
+    original_air_time: '2021-02-16T22:29:00',
+    scheduled_start_time: '2021-02-16T22:35:00',
+  },
+];
+
 const server = setupServer(
   rest.get(`${BACKEND_URL}/tags/`, (req, res, ctx) => {
     return res(ctx.json(tags));
+  }),
+
+  rest.get(`${BACKEND_URL}/rides/`, (req, res, ctx) => {
+    return res(ctx.json(rides));
+  }),
+
+  rest.get(`${BACKEND_URL}/rides/10`, (req, res, ctx) => {
+    return res(ctx.json(rides[0]));
+  }),
+
+  rest.get(`${BACKEND_URL}/rides/10/comments`, (req, res, ctx) => {
+    return res(ctx.json(comments));
   })
 );
 
@@ -54,6 +123,9 @@ it('should accept input in the search field', async () => {
     expect(appBar.getByText(tag.name)).toBeInTheDocument();
     expect(appBar.getByText(`${tag.tag_count} rides`)).toBeInTheDocument();
   });
+
+  // const listbox = within(appBar.getByRole('listbox'));
+  // fireEvent.click(listbox.getByText(tags[0].name));
 });
 
 it('should not allow users to enter separate words in the search', async () => {
